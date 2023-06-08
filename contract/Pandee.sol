71765df71af5e0c18f94b491c5907ab412c73212
PandeeCoin <135799180+PandeeCoin@users.noreply.github.com>
2023-06-09 02:10:15 +0900

    Update Pandee.sol

--- contract/Pandee.sol
+++ contract/Pandee.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import ./ERC20.sol;
-import ./Ownable.sol;
+import "./ERC20.sol";
+import "./Ownable.sol";
 
 contract Pandee is ERC20, Ownable {
     constructor() ERC20("Pandee Coin", "PANDEE") {
